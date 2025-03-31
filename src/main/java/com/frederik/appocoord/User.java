5c8611b20ce9e65569000e23c2e6ecada2e579c0
frederik <termite_pilgrim04@icloud.com>
2025-03-31 18:07:29 +0200

    add fingerprint(no auth)

--- src/main/java/com/frederik/appocoord/User.java
+++ src/main/java/com/frederik/appocoord/User.java
@@ -3,14 +3,25 @@ package com.frederik.appocoord;
 import org.springframework.lang.NonNull;
 
 public class User {
+    @NonNull private String fingerprint;
     @NonNull
     private String name;
     @NonNull
     private String email;
 
-    public User(@NonNull String name, @NonNull String email) {
+    public User(@NonNull String fingerprint, @NonNull String name, @NonNull String email) {
+        this.fingerprint = fingerprint;
         this.name = name;
         this.email = email;
+    }
+
+    @NonNull
+    public String getFingerprint() {
+        return fingerprint;
+    }
+
+    public void setFingerprint(@NonNull String fingerprint) {
+        this.fingerprint = fingerprint;
     }
 
     @NonNull
