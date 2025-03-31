3d94e677dde154c3ff8be5916479317a25331543
frederik <termite_pilgrim04@icloud.com>
2025-03-31 22:02:42 +0200

    seems like the config was deprecated

--- src/main/java/com/frederik/appocoord/RedisConfig.java
+++ src/main/java/com/frederik/appocoord/RedisConfig.java
@@ -3,15 +3,28 @@ package com.frederik.appocoord;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.io.File;
 
 @Configuration
 public class RedisConfig {
+    @Bean
+    public RedisConnectionFactory lettuceConnectionFactory() {
+        String host = isRunningInDocker() ? "redis" : "localhost";
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, 6379));
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         return template;
+    }
+
+    private boolean isRunningInDocker() {
+        return new File("/.dockerenv").exists() || "true".equalsIgnoreCase(System.getenv("RUNNING_IN_DOCKER"));
     }
 }
