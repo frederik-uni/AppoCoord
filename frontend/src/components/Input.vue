bd701156b48ad71509cfdfb94cfcc9da72bfe03a
frederik <termite_pilgrim04@icloud.com>
2025-04-01 10:27:16 +0200

    create components

---
+++ frontend/src/components/Input.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const model = defineModel<string>()
+const props = defineProps<{
+  type: string
+  required: boolean
+}>();
+</script>
+
+<template>
+  <input v-model="model" :type="type" class="border p-2 rounded w-full"  :required="required"/>
+</template>
--- frontend/src/pages/create.vue
+++ frontend/src/pages/create.vue
@@ -1,6 +1,32 @@
 <script setup lang="ts">
+import {reactive} from 'vue'
+import Container from "@/components/Container.vue";
+import Title from "@/components/Title.vue";
+import Submit from "@/components/Submit.vue";
+import Input from "@/components/Input.vue";
+
+const formData = reactive({
+  title: '',
+  description: '',
+  location: '',
+  end: '',
+  uploader: {
+    name: '',
+    email: ''
+  },
+  available_times: []
+})
 </script>
 
 <template>
-  Create
+  <Container>
+    <Title>Create Poll</Title>
+    <Input name="title" type="text" v-model="formData.title" :required="true"/>
+    <Input name="description" type="text" v-model="formData.description" :required="false"/>
+    <Input name="location" type="text" v-model="formData.location" :required="false"/>
+    <Input name="End Poll" type="date" v-model="formData.end" :required="true"/>
+    <Input name="Username" type="text" v-model="formData.uploader.name" :required="true"/>
+    <Input name="Email" type="text" v-model="formData.uploader.email" :required="true"/>
+    <Submit>Share Poll</Submit>
+  </Container>
 </template>
