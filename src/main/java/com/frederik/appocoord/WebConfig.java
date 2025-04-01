531bca35e730ae24ec08cfaad6fe8bc44ac2370f
frederik <termite_pilgrim04@icloud.com>
2025-04-01 19:21:38 +0200

    whyyyyyy?!?!?!

--- src/main/java/com/frederik/appocoord/WebConfig.java
+++ src/main/java/com/frederik/appocoord/WebConfig.java
@@ -2,6 +2,7 @@ package com.frederik.appocoord;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,6 +12,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:[^.]*}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/reply/{spring:[^.]*}")
+                .setViewName("forward:/index.html");
+
     }
 
 }
