f15d78934927f75834c26d5ab2cddb09c49908bd
frederik <termite_pilgrim04@icloud.com>
2025-04-02 16:03:23 +0200

    couldnt find openjdk24 so amazoncorretto24 was used

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -2,4 +2,4 @@
 docker build -t local-amazoncorretto24-alpine -f java/Dockerfile . # --no-cache --progress=plain
 docker build -t local-pnpm-alpine -f pnpm/Dockerfile .
 docker build -t local-redis-alpine -f redis/Dockerfile .
-docker build -t local-maven-openjdk24-alpine -f maven/Dockerfile .
+docker build -t local-maven-amazoncorretto24-alpine -f maven/Dockerfile .
