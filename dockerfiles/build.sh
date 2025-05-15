6c2efd1cb372542a2a107e981c62c1b19feab6ba
frederik <termite_pilgrim04@icloud.com>
2025-05-15 14:32:45 +0200

    add nginx(load balancing)

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -3,3 +3,4 @@ docker build -t local-amazoncorretto24-alpine -f java/Dockerfile . # --no-cache 
 docker build -t local-pnpm-alpine -f pnpm/Dockerfile .
 docker build -t local-redis-alpine -f redis/Dockerfile .
 docker build -t local-maven-amazoncorretto24-alpine -f maven/Dockerfile .
+docker build -t local-nginx-alpine -f nginx/Dockerfile .
