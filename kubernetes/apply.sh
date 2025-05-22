a006ab45e2e70602e1670abb1d643b1c2864a3e4
frederik <termite_pilgrim04@icloud.com>
2025-05-22 13:41:10 +0200

    secrets set by cli

--- kubernetes/apply.sh
+++ kubernetes/apply.sh
@@ -13,11 +13,26 @@ else
 fi
 
 REPLICAS=
+SSL_ENABLED=false
+CERT_PATH=""
+REDIS_PASSWORD=""
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     --replicas)
       REPLICAS="$2"
+      shift 2
+      ;;
+    --ssl)
+      SSL_ENABLED=true
+      shift
+      ;;
+    --cert)
+      CERT_PATH="$2"
+      shift 2
+      ;;
+    --password)
+      REDIS_PASSWORD="$2"
       shift 2
       ;;
     *)
@@ -29,16 +44,24 @@ done
 
 if [[ -z "$REPLICAS" ]]; then
   echo "Error: --replicas flag is required."
-  echo "Usage: $0 --replicas <number>"
+  echo "Usage: $0 --replicas <number> [--ssl] [--cert <path>] [--password <string>]"
   exit 1
 fi
 
+yq e '
+  .stringData.SSL = (env(SSL_ENABLED) == "true" | tostring) |
+  .stringData.CERT_PATH = (env(CERT_PATH) != "" ? env(CERT_PATH) : "/tls") |
+  (env(REDIS_PASSWORD) != "" ? .stringData.REDIS_PASSWORD = env(REDIS_PASSWORD) : del(.stringData.REDIS_PASSWORD))
+' secrets.yml > temp-secrets.yml
+
 minikube mount ../certs:/certs
 kubectl apply -f namespace.yml
-kubectl apply -f secrets.yml
+kubectl apply -f temp-secrets.yml
 kubectl apply -f persistent-volumes.yml
 kubectl apply -f redis-deployment.yml
 yq e ".spec.replicas = $REPLICAS" servers-deployment.yml | kubectl apply -f -
 kubectl apply -f servers-deployment.yml
 kubectl apply -f nginx-configmap.yml
 kubectl apply -f nginx-deployment.yml
+
+rm temp-secrets.yml
