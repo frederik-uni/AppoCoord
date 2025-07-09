a3cd30877a22ab260e4de721dc78819411438286
frederik <termite_pilgrim04@icloud.com>
2025-07-09 23:56:26 +0200

    fix path

--- generate_https_certs.sh
+++ generate_https_certs.sh
@@ -5,6 +5,6 @@ mkdir certs/https
 mkdir certs/redis-tls
 openssl req -x509 -nodes -days 365 \
   -newkey rsa:2048 \
-  -keyout https/privkey.pem \
-  -out https/fullchain.pem \
+  -keyout certs/https/privkey.pem \
+  -out certs/https/fullchain.pem \
   -subj "/CN=localhost"
